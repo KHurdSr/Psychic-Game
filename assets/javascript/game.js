2261709d9fa338aa87994e992a957421d58b0789
khurdsr <korilhurdsr@gmail.com>
2019-01-05 22:39:04 -0800

    homework

---
+++ assets/javascript/game.js
@@ -0,0 +1,21 @@
+
+var computerChoices = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+
+var wins = 0;
+var losses = 0;
+var guesses = 9;
+var guessesLeft = 9;
+var guessedLetters = [];
+var letterToGuess = null;
+
+
+
+var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+
+
+  totalGuesses = 9;
+  guessesLeft = 9;
+  guessedLetters = [];
+
+ 
